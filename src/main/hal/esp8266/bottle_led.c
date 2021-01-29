14616faeb39271367e629a10a2e0a0cb244996e0
Tomoyuki Sakurai <y@trombik.org>
2021-01-29 15:38:22 +0700

    bugfix: fix incorrect usage of the library functions
    also:
    
    - use pwm_stop() instead of setting the duty to zero, which is
      straightforward

--- src/main/hal/esp8266/bottle_led.c
+++ src/main/hal/esp8266/bottle_led.c
@@ -7,24 +7,43 @@
 #define PWM_FREQ    CONFIG_PROJECT_PWM_FREQ_HZ
 #define PWM_T       (1000 * 1000 / PWM_FREQ) // in usec
 #if PWM_T < 20
-#error "PWM_T must me more than 20 usec"
+#error "PWM_T must be more than 20 usec"
 #endif
-#define PWM_CHANNEL (1)
 
-static char tag[] = "bottle_led";
-
-esp_err_t bottle_led_init()
-{
-    return ESP_OK;
-}
+/*
+ * Number of PWM channel to use. must match the length of const variables in
+ * bottle_led_config(). having no plan to support multiple LED strips, this
+ * should always be 1.
+ *
+ * the official documentation is very misleading here: pwm_init() accepts
+ * arrays of arguments except period, while claiming `pin_num` is _GPIO number
+ * of PWM channel_.
+ */
+#define N_PWM_CHANNEL (1)
 
 esp_err_t bottle_led_config(const gpio_num_t gpio_num)
 {
     esp_err_t err = ESP_FAIL;
+    uint32_t duty[N_PWM_CHANNEL] = {
+        0
+    };
 
-    err = pwm_init(PWM_T, 0, PWM_CHANNEL, &gpio_num);
+    const uint32_t pin_num[N_PWM_CHANNEL] = {
+        gpio_num
+    };
+    float phases[N_PWM_CHANNEL] = {
+        0
+    };
+
+    err = pwm_init(PWM_T, duty, N_PWM_CHANNEL, pin_num);
     if (err != ESP_OK) {
         ESP_LOGE(tag, "pwm_init(): %s", esp_err_to_name(err));
+        goto fail;
+    }
+
+    err = pwm_set_phases(phases);
+    if (err != ESP_OK) {
+        ESP_LOGE(tag, "pwm_set_phase(): %s", esp_err_to_name(err));
         goto fail;
     }
 
@@ -37,11 +56,11 @@ fail:
     return err;
 }
 
-esp_err_t bottle_led_update_duty(const uint8_t duty)
+static esp_err_t bottle_led_update_duty(const uint8_t duty)
 {
     esp_err_t err = ESP_FAIL;
 
-    err = pwm_set_duty(PWM_CHANNEL, duty);
+    err = pwm_set_duty(0, duty);
     if (err != ESP_OK) {
         ESP_LOGE(tag, "pwm_set_duty(): %s", esp_err_to_name(err));
         goto fail;
@@ -55,7 +74,28 @@ fail:
     return err;
 }
 
-esp_err_t bottle_led_stop()
+esp_err_t bottle_led_resume()
 {
-    return bottle_led_update_duty(0);
+    esp_err_t err;
+
+    err = pwm_start();
+    if (err != ESP_OK) {
+        ESP_LOGE(tag, "pwm_start(): %s", esp_err_to_name(err));
+        goto fail;
+    }
+fail:
+    return err;
+}
+
+esp_err_t bottle_led_pause()
+{
+    esp_err_t err;
+
+    err = pwm_stop(1 << N_PWM_CHANNEL);
+    if (err != ESP_OK) {
+        ESP_LOGE(tag, "pwm_stop(): %s", esp_err_to_name(err));
+        goto fail;
+    }
+fail:
+    return err;
 }
